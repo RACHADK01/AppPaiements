f48826ddd4e9a89cd0212643885c4a2fefdb1c20
Rachad <300140175@monboreal.ca>
2024-10-19 21:01:39 -0400

    Mise a jour des classes Cartecredit et Program

--- CarteCredit.cs
+++ CarteCredit.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< HEAD
-=======
 using System.Reflection.Metadata;
->>>>>>> dev-cartecredit
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AppPaiements
 {
-<<<<<<< HEAD
-    internal class CarteCredit
-    {
-=======
+
     internal class CarteCredit : Paiement
     {
         public int NumeroCarte { get; set; }
@@ -27,6 +21,6 @@ namespace AppPaiements
         {
             Console.WriteLine($"Carte de crédit - Montant: {Montant}, Description: {Description}, Numéro de carte: {NumeroCarte}");
         }
->>>>>>> dev-cartecredit
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Instances de Carte de Credit
         CarteCredit paiement1 = new CarteCredit(150.00, "Achat de livres", 123456789);
         CarteCredit paiement2 = new CarteCredit(200.00, "Achat d'équipement", 987654321);
         CarteCredit paiement3 = new CarteCredit(50.00, "Repas au restaurant", 456789123);
@@ -11,5 +12,12 @@ internal class Program
         paiement1.AfficherDetails();
         paiement2.AfficherDetails();
         paiement3.AfficherDetails();
+
+        // Instances de Paypal
+        Paypal paiementPaypal1 = new Paypal(300.00, "Abonnement en ligne", "jean@example.com");
+        Paypal paiementPaypal2 = new Paypal(100.00, "Donation", "sophie@example.com");
+
+        paiementPaypal1.AfficherDetails();
+        paiementPaypal2.AfficherDetails();
     }
 }
