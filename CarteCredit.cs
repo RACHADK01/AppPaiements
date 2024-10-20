4b87b532db70ec930c66d74b0904069e02725147
Rachad <300140175@monboreal.ca>
2024-10-19 20:40:00 -0400

    Ajout de la classe CarteCredit dans la branche dev-cartecredit

--- CarteCredit.cs
+++ CarteCredit.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AppPaiements
 {
-    internal class CarteCredit
+    internal class CarteCredit : Paiement
     {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int numeroCarte): base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Carte de crédit - Montant: {Montant}, Description: {Description}, Numéro de carte: {NumeroCarte}");
+        }
     }
 }
